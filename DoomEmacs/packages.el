47defbf6b3d4b8eef60cbbe18acd3b6bdd82d3ff
LucasBrentano <brentano.l@gmail.com>
2022-12-08 13:42:01 -0300

    doom configs

---
+++ DoomEmacs/packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
+
+;; TODO
+(package! gdscript-mode)
+(package! rustic)
+(package! beacon)
+
