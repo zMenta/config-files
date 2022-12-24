28a12f6303bb6208d9bf77666b14f8560defed77
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2022-12-24 17:13:37 -0300

    Update packages.el

--- DoomEmacs/packages.el
+++ DoomEmacs/packages.el
@@ -5,7 +5,13 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
+;; Languages Packages
 (package! gdscript-mode)
 (package! rustic)
+
+;; Visual / Misc Packages
 (package! beacon)
+
+;; Org Packages
 (package! org-superstar)
+(package! org-appear)
