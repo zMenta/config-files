c3035524eafe575197ed1a29698e228aab12aab1
menta <brentano.l@gmail.com>
2023-06-06 14:15:48 -0300

    added ly login screen

--- installDesktop.sh
+++ installDesktop.sh
@@ -15,6 +15,7 @@ install_list=(
 	i3
 	rofi
 	polybar 
+	ly # TUI Login screen
 
 	#Additional tools
 	rofi-calc
@@ -49,7 +50,15 @@ echo " -> Network manager"
 sudo systemctl start NetworkManager
 sudo systemctl enable NetworkManager
 
+echo " -> ly - Login Manager"
+sudo systemctl enable ly.service
+
 # Adding the user to video group, so in polybar can change the backlight levels.
 echo " -> Adding user $USER to video group"
 echo sudo usermod -aG video "$USER"
+
+echo "------------------------------------"
+echo "SCRIPT FINISHED"
+echo "reboot is recommended"
+echo "------------------------------------"
 
