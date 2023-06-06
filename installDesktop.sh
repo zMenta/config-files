d099ee2c04e5173eac531df938279452c7ba1eb0
menta <brentano.l@gmail.com>
2023-06-06 13:11:38 -0300

    installDesktop update

--- installDesktop.sh
+++ installDesktop.sh
@@ -31,21 +31,25 @@ echo "------------------------------------"
 
 
 echo "-- Updating the sytem --"
-# yay -Syu --noconfirm
-
+yay -Syu --noconfirm
 
 echo "-- Installation Began --"
 for value in "${install_list[@]}"; do
 	eval yay -S "$value" --noconfirm
 done
 
+echo "-- Installing FiraMonoNerdFont --"
+echo " -> /usr/share/fonts/FiraMonoNerdFont"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip -P "$PWD"
+sudo mkdir /usr/share/fonts/FiraMonoNerdFont
+sudo unzip FiraMono.zip -d /usr/share/fonts/FiraMonoNerdFont
 
 echo "-- Packages Configuration --"
-
 echo " -> Network manager"
 sudo systemctl start NetworkManager
 sudo systemctl enable NetworkManager
 
+# Adding the user to video group, so in polybar can change the backlight levels.
 echo " -> Adding user $USER to video group"
 echo sudo usermod -aG video "$USER"
 
