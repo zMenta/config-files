481de48ebe39e5581f83cb72e580ca8d65828119
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2023-06-17 16:48:06 -0300

    better cmp keybinds |  lsp.lua

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -16,11 +16,12 @@ end)
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Shift-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- <CR> = enter/return key.
   ["<C-Space>"] = cmp.mapping.complete(),
 })
+
 
 lsp.setup_nvim_cmp({
   mapping = cmp_mappings
