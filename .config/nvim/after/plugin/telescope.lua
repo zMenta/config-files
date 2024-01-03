a2a8bde8aed13dba96c8f8e98d68a2a37ebe1cda
moog <brentano.l@gmail.com>
2024-01-03 10:17:09 -0300

    neovim Telescope additional keybinds

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,16 +1,18 @@
 local builtin = require('telescope.builtin')
 
--- File Related --
+-- Find Related --
 vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = "Find files in current git project" })
 vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = "Find File"})
-vim.keymap.set('n', '<leader>sf', builtin.live_grep, { desc = "Grep Files"})
 
 
 -- Search Related --
-vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = "Search Commands" })
-vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = "Search Keymaps" })
-vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = "Grep current buffer" })
+vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = "Commands" })
+vim.keymap.set('n', '<leader>sh', builtin.command_history, { desc = "Command History" })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = "Keymaps" })
+vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = "Grep Current Buffer"})
+vim.keymap.set('n', '<leader>sf', builtin.live_grep, { desc = "Grep Files"})
+vim.keymap.set('n', '<leader>sm', builtin.man_pages, { desc = "Man Pages"})
 
 
 vim.keymap.set('n', '<leader>x', builtin.diagnostics, { desc = "Telescope Diagnostics" })
-
+vim.keymap.set('n', '<leader>t', builtin.treesitter, { desc = "Treesitter Symbols" })
