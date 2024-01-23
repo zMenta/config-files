6690c48868e5bc9cd3c644455328017fa975da7a
moog <brentano.l@gmail.com>
2024-01-22 21:43:02 -0300

    created scripts directory | Created openDocument.sh

---
+++ scripts/openDocument.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Dependencies: fd, zathura, rofi
+
+dir_to_search="/home"
+if [ -d "$1" ]; then
+    dir_to_search=$1
+fi
+
+search_result=$( fd . $dir_to_search -e pdf | rofi -dmenu -matching fuzzy -i -p "Open pdf" ) 
+
+if [ -n "$search_result" ]; then
+    # echo "'$search_result'"
+    zathura "$search_result"
+fi
