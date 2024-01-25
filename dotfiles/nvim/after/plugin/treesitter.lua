3620972554fb3161a4c7559c012bb0ca1be5a560
moog <brentano.l@gmail.com>
2024-01-25 20:15:51 -0300

    nvim treesitter cleanup

--- dotfiles/nvim/after/plugin/treesitter.lua
+++ dotfiles/nvim/after/plugin/treesitter.lua
@@ -1,23 +1,11 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "gdscript", "godot_resource" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "gdscript", "godot_resource" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  -- Automatically install parser when no parser is found for file.
   auto_install = true,
-
 
   highlight = {
     enable = true,
-
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
   },
 }
