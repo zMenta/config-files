3fe59cd5a58854ce35072e4413e6484166ed0351
Moog <brentano.l@gmail.com>
2024-04-26 20:14:26 -0300

    scripts | updated download music script

--- scripts/downloadMusic.sh
+++ scripts/downloadMusic.sh
@@ -7,5 +7,8 @@ download_path=~/Sync/Music
 music_url=$(rofi -dmenu -matching fuzzy -i -p "Download music to -> $download_path") 
 
 if [ -n "$music_url" ]; then
-   yt-dlp -x -f bestaudio -P $download_path $music_url
+    if [ ! -d $download_path ]; then
+        mkdir -p $download_path
+    fi
+    yt-dlp -x -f bestaudio -P $download_path $music_url
 fi
