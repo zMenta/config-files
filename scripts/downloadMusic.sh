4159191d67906cde0bfacc379828aed90360a2fe
Moog <brentano.l@gmail.com>
2024-04-27 20:10:08 -0300

    added notifications with dunst | downloadMusic.sh

--- scripts/downloadMusic.sh
+++ scripts/downloadMusic.sh
@@ -8,7 +8,14 @@ music_url=$(rofi -dmenu -matching fuzzy -i -p "Download music to -> $download_pa
 
 if [ -n "$music_url" ]; then
     if [ ! -d $download_path ]; then
-        mkdir -p $download_path
+        notify-send -u normal "$download_path not found" "Creating path..."
+        if ! mkdir -p $download_path ; then
+            notify-send -u critical "Error" "Unable to create directory $download_path"
+        fi
     fi
-    yt-dlp -x -f bestaudio -P $download_path $music_url
+    if yt-dlp -x -f bestaudio -P $download_path $music_url ; then
+        notify-send -u normal "Music Downloaded" "$music_url"
+    else
+        notify-send -u critical "Failed to download music" "$music_url"
+    fi
 fi
