e52ab0636f3961f864d776c24357d5006673997a
Moog <brentano.l@gmail.com>
2024-04-28 14:01:14 -0300

    created snippet script

---
+++ scripts/snippets/snippet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Dependencies: xdotool, fd, rofi
+
+dir_to_search=~/.scripts/snippets/snippets
+if [ -d "$1" ]; then
+    dir_to_search=$1
+fi
+
+search_result=$( fd . $dir_to_search | rofi -dmenu -matching fuzzy -i -p "Select a snippet" ) 
+
+if [ -n "$search_result" ]; then
+    xdotool type "$(cat $search_result)"
+fi
