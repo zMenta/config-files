1bcd56025320329b51091cadb8b5ec63fd99e97d
zMenta <brentano.l@gmail.com>
2025-02-10 18:03:29 -0300

    plugins update

--- dotfiles/nvim/lua/plugins/auto_pairs.lua
+++ dotfiles/nvim/lua/plugins/auto_pairs.lua
@@ -3,5 +3,10 @@ return {
 		'windwp/nvim-autopairs',
 		event = "InsertEnter",
 		config = true,
+	},
+	{
+		-- Auto pairs for web tags, i.e. <tag> </tag>
+		"windwp/nvim-ts-autotag",
+		opts = {},
 	}
 }
