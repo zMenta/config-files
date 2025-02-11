66e96a720b5b992e0e9aaa8b39f0b1fc8b6d7e41
zMenta <brentano.l@gmail.com>
2025-02-10 21:03:31 -0300

    added dap plugin

---
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -0,0 +1,74 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+			"leoluz/nvim-dap-go", -- Configures DAP to be used with go
+			"nvim-neotest/nvim-nio", -- required for dap UI
+			"williamboman/mason.nvim", -- Mason for easy DAP servers download
+		},
+		config = function()
+			local dap = require "dap"
+			local ui = require "dapui"
+
+			require("dapui").setup()
+			require("dap-go").setup()
+			require("nvim-dap-virtual-text").setup()
+
+			-------------
+			-- Keymaps --
+			-------------
+			vim.keymap.set("n", "<space>db", dap.toggle_breakpoint, { desc = "Dap Toggle Breakpoint"} )
+			vim.keymap.set("n", "<space>gb", dap.run_to_cursor, { desc = "Dap run code until the cursor"} )
+
+			-- Eval var under cursor
+			vim.keymap.set("n", "<space>de", function()
+				require("dapui").eval(nil, { enter = true })
+			end, { desc = "Dap Evaluate Expression" })
+
+			vim.keymap.set("n", "<F1>", dap.continue, { desc = "Dap Continues" })
+			vim.keymap.set("n", "<F2>", dap.step_into, { desc = "Dap Step Into" })
+			vim.keymap.set("n", "<F3>", dap.step_over, { desc = "Dap Step Over" })
+			vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Dap Step Out" })
+			vim.keymap.set("n", "<F5>", dap.step_back, { desc = "Dap Step Back" })
+			vim.keymap.set("n", "<F12>", dap.restart, { desc = "Dap Restart" })
+
+			------------------
+			-- Godot Config --
+			------------------
+			-- In Godot settings check if the port matches:
+			--    Editor -> Editor Settings -> Debug Adapter, under Network section.
+			dap.adapters.godot = {
+				type = "server",
+				host = '127.0.0.1',
+				port = 6006,
+			}
+
+			dap.configurations.gdscript = {
+				{
+					type = "godot",
+					request = "launch",
+					name = "Launch scene",
+					project = "${workspaceFolder}",
+					launch_scene = true,
+				}
+			}
+
+			-- Opens and closes dapui, dap  automatically when dap is started or terminated
+			dap.listeners.before.attach.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				ui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				ui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				ui.close()
+			end
+		end,
+	},
+}
+
