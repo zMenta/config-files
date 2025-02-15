ff51e0d6e53f27cce6c6620276d811a938681818
zMenta <brentano.l@gmail.com>
2025-02-15 18:50:55 -0300

    nvim render_markdown config update

--- dotfiles/nvim/lua/plugins/render_markdown.lua
+++ dotfiles/nvim/lua/plugins/render_markdown.lua
@@ -4,6 +4,15 @@ return {
 		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
-		opts = {},
+		opts = {
+			indent = {
+				enabled = true
+			},
+			heading = {
+				width = 'block',
+				left_pad = 0,
+				right_pad = 2,
+			},
+		},
 	}
 }
