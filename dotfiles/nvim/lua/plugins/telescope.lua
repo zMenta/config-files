e3291290e01879bf96c766446b9b01c04d567885
zMenta <brentano.l@gmail.com>
2025-03-29 19:34:44 -0300

    updated mislabeled keybind

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -27,7 +27,7 @@ return {
 			-- Search Related --
 			vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = "Commands" })
 			vim.keymap.set('n', '<leader>sH', builtin.command_history, { desc = "Command History" })
-			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "Command History" })
+			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = "Help Tags" })
 			vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = "Keymaps" })
 			vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = "Grep Current Buffer" })
 			vim.keymap.set('n', '<leader>sf', builtin.live_grep, { desc = "Grep Files" })
