f9c08810d2d053c8044ac0e3084b791d0fd74ebb
menta <brentano.l@gmail.com>
2026-01-29 14:46:17 -0300

    adding systemd scripts

---
+++ scripts/.scripts/batteryNotification.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+battery="BAT0"
+lowLevel=25
+highLevel=80
+currentCharge=`cat /sys/class/power_supply/${battery}/capacity`
+currentStatus=`cat /sys/class/power_supply/${battery}/status`
+tempFile=~/.config/systemd/tempfiles/tempBatteryFile
+
+notify() {
+	if [ ! -e $tempFile ]; then
+		notify-send --urgency "$1" "󰂃 Battery" "$2" --expire-time 15000
+		touch $tempFile
+	fi
+}
+
+if (( $currentCharge >= $highLevel )) && [ $currentStatus == "Charging" ]; then
+	notify "normal" "Battery is above $highLevel%"
+elif (( $currentCharge <= $lowLevel )) && [ $currentStatus == "Discharging" ]; then
+	notify urgent "Low Battery!"
+elif [ -e $tempFile ]; then
+	echo "removing temp file"
+	rm $tempFile
+fi
+
