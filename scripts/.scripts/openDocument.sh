11c2ae8f55950f5fce21edec1c82a60bb44fbbd5
zMenta <brentano.l@gmail.com>
2026-09-18 12:00:49 -0300

    rofi themes and key-handler update

--- scripts/.scripts/openDocument.sh
+++ scripts/.scripts/openDocument.sh
@@ -6,7 +6,11 @@ if [ -d "$1" ]; then
     dir_to_search=$1
 fi
 
-search_result=$( fd . $dir_to_search -e pdf | rofi -dmenu -matching fuzzy -i -p "Open pdf" ) 
+# search_result=$( fd . $dir_to_search -e pdf | rofi -dmenu -matching fuzzy -i -p "Open pdf" ) 
+
+search_result=$(fd . "$dir_to_search" -e pdf -a | while read -r file; do
+    echo -en "${file}\0icon\x1fthumbnail://${file}\n"
+	done | rofi -dmenu -matching fuzzy -i -p "Open pdf" -show-icons -config ~/.config/rofi/bigicon_list_preview.rasi)
 
 if [ -n "$search_result" ]; then
     zathura "$search_result"
